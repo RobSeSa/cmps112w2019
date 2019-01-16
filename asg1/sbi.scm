7108308a7657fa4859e22e3d934c5f40f8855165
Robert Sato <rssato@ucsc.edu>
2019-01-15 22:13:04 -0800

    working label, variable, function, eval-expr

--- asg1/sbi.scm
+++ asg1/sbi.scm
@@ -53,26 +53,9 @@
     (printf "(~n")
     (map (lambda (line) (printf "~s~n" line)) program)
     (printf ")~n"))
-;    (printf "(car program) = ~s~n" (car program))
-;    (map (lambda (output) (printf "(car program) = ~s~n" (car output))) program)
-;    (map (lambda (output) (printf "(cdr program) = ~s~n" (cdr output))) program)
-;    (map (lambda (output) (printf "(cadr program) = ~s~n" (cadr output))) program)
-;    (map (lambda (output) (printf "(length output) = ~s~n" (length output))) program)
-;    (map (lambda (output) (printf "(caadr program) = ~s~n" (caadr output))) program)
-;    (print-each-car program)
-;    (dump-stdin))
 
-
-(define (print-each-car program)
-   (if (not (null? program)) 
-        [(printf "(car program = ~s~n" (car program)) 
-         (printf "(cdr program = ~s~n" (cdr program)) 
-         (printf "(length (car program) = ~s~n" (length (car program)))
-         (printf "(length (car program) = ~s~n" (length (car program)))
-         (print-each-car (cdr program))]
-        [(printf "eof was reached")]
-   )
-)
+;;
+;;
 
 ;simple print function
 (define (show label it)
@@ -81,29 +64,114 @@
     (display it)
     (newline)
 )
-
-(define *label-table* (make-hash))
+;simple print for hash tables
 (define (print-table table)
     (hash-for-each table (lambda (key value) (show key value)))
 )
+;simple print function that prints cars
+(define (print-each-car program)
+    (when (not (null? program))
+        (show "car program" (car program))
+        (print-each-car (cdr program))
+    )
+)
+
+;; label definitions
+(define *label-table* (make-hash))
 (define (create-label-table program)
     (when (not (null? program))
-        ;when the number of objects in the first line in the list is > 2
-        ;this implies that there is a line number, label, and statement
-        ;;(when (> (length (car program)) 2)
-        (cond [(symbol? (cadr (car program))) 
-            ;store the 2nd object in the line as the key and the line number as the value
-            (hash-set! *label-table* (cadr (car program)) (car (car program)))])
-        ;repeat recursively on the rest of the lines
+        (when (> (length (car program)) 1)
+            (cond [(symbol? (cadr (car program))) 
+                ;store the 2nd object in the line as the key and the line number as the value
+                (hash-set! *label-table* (cadr (car program)) program)])
+            ;repeat recursively on the rest of the lines
+        )
         (create-label-table (cdr program))
     )
 )
 
+(define *array-table* (make-hash))
+;; variable definitions
+(define *variable-table* (make-hash))
+(define (variable-get key)
+        (hash-ref *variable-table* key))
+(define (variable-put! key value)
+        (hash-set! *variable-table* key value))
+(for-each
+    (lambda (pair)
+            (variable-put! (car pair) (cadr pair)))
+    `(
+        (nan     ,(/ 0.0 0.0))
+        (eof     0.0)
+        (pi      ,(acos -1.0))
+        (e       ,(exp 1.0))
+
+     ))
+;; function definitions
+(define *function-table* (make-hash))
+(define (function-get key)
+        (hash-ref *function-table* key))
+(define (function-put! key value)
+        (hash-set! *function-table* key value))
+(for-each
+    (lambda (pair)
+            (function-put! (car pair) (cadr pair)))
+    `(
+
+        (abs     ,abs)
+        (acos    ,acos)
+        (asin    ,asin)
+        (atan    ,atan)
+        (ceil    ,ceiling)
+        (cos     ,cos)
+        (exp     ,exp)
+        (floor   ,floor)
+        (log     ,log)
+        (log10   ,(lambda (x) (/ (log x) (log 10.0))))
+        (log2   ,(lambda (x) (/ (log x) (log 2.0))))
+        (round   ,round)
+        (sin     ,sin)
+        (sqrt    ,sqrt)
+        (tan     ,tan)
+        (trunc   ,truncate)
+        (+       ,+)
+        (-       ,-)
+        (*       ,*)
+        (/       ,/)
+        (%       ,(lambda (x y) (- x (* (div x y) y))))
+        (^       ,expt)
+        ;; unecessary functions
+        (log10_2 0.301029995663981195213738894724493026768189881)
+        (sqrt_2  1.414213562373095048801688724209698078569671875)
+        (div     ,(lambda (x y) (floor (/ x y))))
+        (quot    ,(lambda (x y) (truncate (/ x y))))
+        (rem     ,(lambda (x y) (- x (* (quot x y) y))))
+
+     ))
+
+;; takes in an expression
+(define (eval-expr statement)
+    (cond [(number? statement) (+ statement 0.0)]
+          [(symbol? statement) (cond [(not (eq? (hash-ref *variable-table* statement 0) 0)) 
+					(hash-ref *variable-table* statement 0)]
+					[(not (eq? (hash-ref *array-table* statement 0)))
+					(hash-ref *array-table* statement 0)]
+					(else 0))
+	  ]
+	  [(pair? statement) (apply (hash-ref *function-table* (car statement) #f)
+                                    (map eval-expr (cdr statement)))]
+    )
+)
+
+
 (define (execute-program program)
-    (display "in execute-program")
-    (newline)
     (create-label-table program)
     (print-table *label-table*)
+    (print-table *variable-table*)
+    (print-table *function-table*)
+    ;(print-each-car program)
+    (display (eval-expr '(+ e)))
+    (newline)
 )
 
 (define (main arglist)
@@ -111,7 +179,7 @@
         (usage-exit)
         (let* (  (sbprogfile (car arglist))
                  (program (readlist-from-inputfile sbprogfile)))
-;              (write-program-by-line sbprogfile program)
+              ;(write-program-by-line sbprogfile program)
               (execute-program program)
         )
     )
