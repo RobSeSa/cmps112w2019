fc3c8b4f18127c5038067ba92fd69b4d12ab0a0f
Huanlei Wu <hwu43@ucsc.edu>
2019-03-07 15:57:42 -0800

    finished the function for finding the path

--- functions.pl
+++ functions.pl
@@ -4,14 +4,36 @@
 not( X ) :- X, !, fail.
 not( _ ).
 
-fly( from, to ) :-
-   isFlight( from, to).
+fly( From, To ) :-
+   isFlight( From, To, [From] ).
 
 % sees if there is a viable path
-isFlight( from, to ) :-
-   flight( from, to, _, _ ).
+isFlight( From, To , Tried ) :-
+   flight( From, To, _ ),
+   append( Tried, [To], Path ), 
+   printSched( Path ).
+   %nl, write( From ), write(' '), write( To ).
 
-isFlight( from, to ) :-
-   flight( from, layover ), isFlight( layover, to ).
+isFlight( From, To , Tried ) :-
+   not( member( To, Tried ) ),
+   flight( From, Layover, _ ), 
+   not( member( Layover, Tried ) ),
+   isFlight( Layover, To, [Layover|Tried] ).
+   %nl, write( From ), write(' '), write( Layover ).
 
 % calculate the time
+%radians( From, To ) :-
+   %airport( From, _, degmin( Deg1, Min1 ), degmin( Deg2, Min2 ) ),
+   %airport( To, _, degmin( Deg3, Min3 ), degmin( Deg4, Min4 ) ),
+   %Rad1 is (Deg1 + Min1 div 60) + pi div 180,
+   %write( Rad1 ).
+   %rad2 is ,
+   %rad3 is ,
+   %rad4 is , 
+
+printSched( [Head] ) :-
+   write( Head ).
+
+printSched( [Head|Tail] ) :-
+   write( Head ), nl,
+   printSched( Tail ).
