eac718a2f4eeecbc0e31e19a22aa5c7f7e5b5cad
Huanlei Wu <hwu43@ucsc.edu>
2019-03-12 00:05:44 -0700

    some working, others don't work; ex: sea to bos doesn't work

--- functions.pl
+++ functions.pl
@@ -4,96 +4,76 @@
 not( X ) :- X, !, fail.
 not( _ ).
 
+% returns the first element in the list
+pop( [Head|_], Value ) :-
+   Value = Head.
 
+% returns the second element in the list
+pop2( [_|Tail], Value ) :-
+   pop( Tail , Value ).
+
+% sees if there is a viable Path
 fly( From, To ) :-
-   isFlight( From, To, [From] ).
+   isFlight( From, To, [From], [From] ).
 
-%if flight( From -> To ) true
-isFlight( From, To , Tried ) :-
-   write('Checking for direct flight from '), 
-   write( From ), write(' to '), write( To ), nl,
+% if flight( From -> To ) true
+isFlight( From, To, Tried, _ ) :-
    flight( From, To, _ ),
    append( Tried, [To], Path ), 
-   printSched( Path , 0).
+   nl, printSched( Path , 0).
 
-%elseif flight( From -> Temp1 -> To ) true
-isFlight( From, To, Tried) :-
-   nl, write('Start testing for From -> temp1 -> To.. '), nl,
-   flight( From, Temp1, _),
-   write('From = '), write(From), 
-   write('; Temp1 = '), write(Temp1),
-   write('; To = '), write(To), 
-   write('; Tried = '), write(Tried), nl,
-   not( member( Temp1, Tried ) ),
-   write('flight temp1 temp2..'), nl,
-   flight( Temp1, Temp2, _),
-   write('From = '), write(From), 
-   write('; Temp1 = '), write(Temp1),
-   write('; Temp2 = '), write(Temp2),
-   write('; To = '), write(To), 
-   write('; Tried = '), write(Tried), nl,
-   write('Checking if '), write(Temp2),
-   write(' not a member of tried.. '),nl,
-   not( member( Temp2, Tried ) ),
-   write('Checking the time..'), nl,
-   checkTime( From, Temp1, Temp2 ),
-   write('Comparing Temp2 and To'), nl,
-   Temp2 = To,
-   write('Flight exists with one layover.. '), nl,
-   write('append1.. '), nl,
-   append( Tried, [Temp1], Path ), 
-   write('append2.. '), nl,
-   append( Path, [To], Final ), 
-   write('printSched.. '), nl,
-   printSched( Final , 0).
+% there is a layover in the flight
+isFlight( From, To, Tried, Flights ) :-
+   not( member( To, Tried ) ),
+   pop( Tried, Head ),
+   Head \= From,
+   flight( From, Layover, _ ),
+   write('To '), write(To), nl,
+   write('From '), write(From), nl,
+   write('Layover '), write(Layover), nl,
+   not( member( Layover, Tried ) ),
+   pop2( Flights, PrevFlight ),
+   write('PreFlight '), write(PrevFlight), nl,
+   write(Tried),nl, !,
+   checkTime( PrevFlight, From, Layover ),
+   append( Tried, [Layover], Path ),
+   write(Path), nl,
+   isFlight( Layover, To, Path, [Layover|Flights] ).
 
-%else
-isFlight( From, To, Tried) :-
-   nl, write('Start testing in else clause.. '), nl,
-   flight( From, Temp1, _),
-   write('From = '), write(From), 
-   write('; Temp1 = '), write(Temp1),
-   write('; To = '), write(To), 
-   write('; Tried = '), write(Tried), nl,
-   not( member( Temp1, Tried ) ),
-   flight( Temp1, Temp2, _),
-   write('From = '), write(From), 
-   write('; Temp1 = '), write(Temp1),
-   write('; Temp2 = '), write(Temp2),
-   write('; To = '), write(To), 
-   write('; Tried = '), write(Tried), nl,
-   not( member( Temp2, Tried ) ),
-   write('Checking the time..'), nl,
-   checkTime( From, Temp1, Temp2 ),
-   append( Tried, [Temp1], Path ), 
-   append( Path, [Temp2], Final ), 
-   write('Adding two layovers..'), nl, nl,
-   isFlight( Temp2, To, Final ).
+% there is a layover in the flight
+% setting up for the next isFlight expression
+isFlight( From, To, Tried, Flights ) :-
+   not( member( To, Tried ) ),
+   flight( From, Layover, _ ),
+   append( Tried, [Layover], Path ),
+   %write('To '), write(To), nl,
+   %write('From '), write(From), nl,
+   write('Layover init '), write(Layover), nl,
+   %write(Path), nl,
+   isFlight( Layover, To, Path , [Layover|Flights]).
 
-%checks if the flight From to Layover makes it before Layover to To
+% checks if the flight From to Layover makes it before Layover to To
 checkTime( From, Layover, To ) :-
-   minutes( From, Layover, MinA ),
-   time( From, Layover, TripA ),
-   minutes( Layover, To, MinB ),
-   AToB is MinA + TripA + 30,
-   write('Comparing flight '), write(Layover),
-   write(' to '), write(To), 
-   write(' departure time: '), write(MinB),
-   write(' with '), write(From),
-   write(' to '), write(Layover), 
-   write(' departure + flight + 30: '), write(AToB), nl,
-   MinB >= AToB.
+   minutes( From, Layover, DepartTimeA ),
+   write( From ), write(' to '), write(Layover), write(' is at '), write(DepartTimeA), nl,
+   time( From, Layover, TripDuration ),
+   write('takes this long: '), write(TripDuration), nl,
+   minutes( Layover, To, DepartTimeB ),
+   write( Layover ), write(' to '), write(To), write(' is at '), write(DepartTimeB), nl,
+   AToB is DepartTimeA + TripDuration + 30,
+   write(AToB), nl,
+   AToB =< DepartTimeB.
 
-%grabs time converted to minutes
+% converts hours and minutes to minutes
 minutes( From, To, Minutes ) :-
    flight( From, To, time( Hour, Min ) ),
    Minutes is Hour * 60 + Min.
 
-%grabs hours and minutes
-hoursMins( From, To, Hours, Minutes ) :-
-   flight( From, To, time( Hour, Min ) ),
-   Hours is Hour,
-   Minutes is Min.
+% calculates the duration of flight
+time( From, To, Time ) :-
+   radians( From, To, Distance ),
+   Hours is Distance / 500,
+   Time is round( Hours * 60 ).
 
 % calculates the radians
 radians( From, To , Distance) :-
@@ -113,50 +93,42 @@ haversine_radians( Lat1, Lon1, Lat2, Lon2, Distance ) :-
       + cos( Lat1 ) * cos( Lat2 ) * sin( Dlon / 2 ) ** 2,
    Dist is 2 * atan2( sqrt( A ), sqrt( 1 - A )),
    Distance is Dist * 3959.
-   %write(Distance).
 
-% calculates the duration of flight
-time( From, To, Time ) :-
-   radians( From, To, Distance ),
-   Hours is Distance / 500,
-   Time is round( Hours * 60 ).
-   %write( Time ), nl.
+% grabs hours and minutes from a flight
+hoursMins( From, To, Hours, Minutes ) :-
+   flight( From, To, time( Hour, Min ) ),
+   Hours is Hour,
+   Minutes is Min.
 
+% converts minutes into hours and minutes
 convertTime( Minutes, HoursO, MinsO ) :-
    HoursO is Minutes div 60,
    MinsO is Minutes mod 60.
 
+% changes all characters in string to uppercase
 to_upper( Lower, Upper) :-
    atom_chars( Lower, Lowerlist),
    maplist( lower_upper, Lowerlist, Upperlist),
    atom_chars( Upper, Upperlist).
 
+% prints out the trip in desired format
 print_trip( Action, Code, Name, time( Hour, Minute)) :-
    to_upper( Code, Upper_code),
    format( "%-6s  %3s  %-16s  %02d:%02d",
            [Action, Upper_code, Name, Hour, Minute]),
    nl.
 
-pop( [Head|_], Value ) :-
-   Value = Head.
-
-addTime(Hours, Minutes, Total) :-
-   Total is Hours * 60 + Minutes.
-
-
-printSched( [Head], _ ) :-
-   write(Head), nl.
-
+% prints the viable flight schedule out
 printSched( [Dep|Tail] , Current) :-
    pop( Tail, Arr ),
    airport( Dep, DepName, _, _ ),
    hoursMins( Dep, Arr, HourD, MinsD ),
-   addTime(HourD, MinsD, DepMins),
-   Current =< DepMins,
+   DepMins is HourD * 60 + MinsD,
+   DepMins >= Current,
 
    airport( Arr, ArrName, _, _ ),
    minutes( Dep, Arr, MinA ),
-   Current =< MinA,
+   MinA > Current,
 
    time( Dep, Arr , TripA ),
    ArrTime is MinA + TripA,
@@ -166,16 +138,5 @@ printSched( [Dep|Tail] , Current) :-
    print_trip( arrive, Arr, ArrName, time( ArrHours, ArrMins)),
    printSched( Tail, TimeCurr ).
 
-
-/*
-test :-
-   print_trip( depart, nyc, 'New York City', time( 9, 3)),
-   print_trip( arrive, lax, 'Los Angeles', time( 14, 22)).
-
-printSched( [Head] ) :-
-   write( Head ), nl.
-
-printSched( [Head|Tail] ) :-
-   write( Head ), write(' -> '),
-   printSched( Tail ).
-*/
+printSched( [_], _ ) :-
+   nl.
